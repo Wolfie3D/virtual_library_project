397de7c9df0f7ced6404b0dede1ef6f1cfa53491
Chris Kamau <kwangechi3@gmail.com>
2024-01-31 02:36:30 -0800

    add the models: Author and Books

--- books/models.py
+++ books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    age = models.IntegerField()
+    country = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
